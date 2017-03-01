93512e4f4a29338ad3adaa4db47da63a288a3be2
thiagotmb <tmb0710@gmail.com>
2017-02-28 23:01:33 -0500

    Adding code to send user sendUserAccessMail

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,19 @@
 
-Parse.Cloud.define('hello', function(req, res) {
+Parse.Cloud.define('hello', function (req, res) {
+
   res.success('Hi');
+});
+
+
+Parse.Cloud.define('sendUserRegisterConfirmationMail', function (req, res) {
+
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("username", req.params.email);
+  query.equalTo("objectId", req.params.objectId);
+  query.find().then(function (results) {
+    return res.success(results);
+  }, function (error) {
+    return res.error('User not found')
+  });
+
 });
